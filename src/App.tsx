050a4e0f2e8a089ce910b6413c93e8e66a5e0442
prathmesh <prathmeshpol61@gmail.com>
2024-03-14 13:48:17 +0530

    add : search feature component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Accordion from "./components/Accordion/Accordion";
+import Search from "./components/Search/Search";
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col gap-20">
       <Accordion />
-    </>
+      <Search />
+    </div>
   );
 }
 
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+type TArray = {
+  color: string;
+  value: string;
+}[];
+const Search = () => {
+  const [value, setValue] = useState<string>("");
+  const array: TArray = [
+    {
+      color: "red",
+      value: "#f00",
+    },
+    {
+      color: "green",
+      value: "#0f0",
+    },
+    {
+      color: "blue",
+      value: "#93C5FD",
+    },
+    {
+      color: "cyan",
+      value: "#0ff",
+    },
+    {
+      color: "magenta",
+      value: "#f0f",
+    },
+    {
+      color: "yellow",
+      value: "#ff0",
+    },
+    {
+      color: "black",
+      value: "#000",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="w-[500px]  border-gray-300 border-2 outline-none rounded-lg py-1 px-4">
+        <input
+          type="text"
+          className="border-none outline-none w-full text-black placeholder:text-gray-400"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+      <div className="max-w-[600px] flex flex-wrap gap-8">
+        {array
+          .filter((item) =>
+            item.color.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+          )
+          .map((item) => (
+            <div className={`bg-[${item.value}]`}>{item.color}</div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
