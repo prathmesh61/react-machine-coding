de4a38574f37480c9cf826d4305452d981cc1e2a
prathmesh <prathmeshpol61@gmail.com>
2024-03-14 14:22:39 +0530

    add : stop-watch component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Accordion from "./components/Accordion/Accordion";
 import Search from "./components/Search/Search";
+import StopWatch from "./components/StopWatch/StopWatch";
 
 function App() {
   return (
     <div className="flex flex-col gap-20">
       <Accordion />
       <Search />
+      <StopWatch />
     </div>
   );
 }
---
+++ src/components/StopWatch/StopWatch.tsx
@@ -0,0 +1,38 @@
+import { useRef, useState } from "react";
+
+const StopWatch = () => {
+  const [timer, setTimer] = useState<number>(0);
+  let timeRef = useRef<number | null>(null);
+  const startTimer = () => {
+    if (!timeRef.current) {
+      timeRef.current = setInterval(() => {
+        setTimer((prev) => prev + 1);
+      }, 1000);
+    }
+  };
+  const stopTimer = () => {
+    clearInterval(timeRef.current!);
+    timeRef.current = null;
+  };
+  return (
+    <div className="flex flex-col gap-8">
+      <h1 className="font-bold text-3xl">{timer}</h1>
+      <div className="flex gap-4">
+        <button
+          className="inline-block py-1 px-4 bg-black text-white"
+          onClick={startTimer}
+        >
+          Start
+        </button>
+        <button
+          className="inline-block py-1 px-4 bg-red-500 text-white"
+          onClick={stopTimer}
+        >
+          Stop
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default StopWatch;
